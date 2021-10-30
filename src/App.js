e4b2d12e51235102f58adca2b60af109f7cae86c
enasnasr <enasnasr2281995@gmail.com>
2021-10-30 12:35:43 +0200

    'first-init'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from "./Data";
+const App = () => {
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
 
-function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count < 15) {
+      amount = 14;
+    }
+
+    setText(data.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center m-5">Generte Your Text</h1>
+      <form className=" m-auto" onSubmit={handleSubmit}>
+        <div className="form-group d-flex justify-content-center align-items-center ">
+          <label className="me-3" htmlFor="Count">
+            Paragraph:
+          </label>
+          <input
+            className="form-control"
+            id="count"
+            type="number"
+            value={count}
+            onChange={(e) => {
+              setCount(e.target.value);
+            }}
+          />
+          <button className="btn">generate</button>
+        </div>
+      </form>
+      <div className="parag m-auto">
+        {text.map((parag, index) => {
+          return <p key={index}>*{parag}</p>;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
